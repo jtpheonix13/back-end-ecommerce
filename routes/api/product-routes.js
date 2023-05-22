db7bf86a65678a4fc2b7706dcaaa1bff2cbb3332
jtpheonix13 <jgtihunt@comcast.net>
2023-05-22 17:41:02 -0500

    testing the app

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -36,7 +36,7 @@ router.get('/:id', async (req, res) => {
 
     res.status(200).json(singleProduct);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
   
 });
@@ -130,7 +130,7 @@ router.delete('/:id', async (req, res) => {
 
     res.status(200).json({message: "This product has been deleted."});
   } catch(err) {
-    res.status(400).json(err);
+    res.status(500).json(err);
   }
 });
 
