f315cefa6aa0c9d66b485099a2f368dde3f157f9
jtpheonix13 <jgtihunt@comcast.net>
2023-05-21 22:33:02 -0500

    finished the tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -79,7 +79,7 @@ router.delete('/:id', async (req, res) => {
     }
     res.status(200).json(deleteCategory);
   } catch (err) {
-    res.status(400).json(err);
+    res.status(400).json({message: "This category has been deleted."});
   }
 });
 
